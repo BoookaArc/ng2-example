54b0bac6e287772564a1eb8bcd85d9686ee39df2
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 19:58:04 +0300

    example/focus-cell-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+import { GridModel, Grid } from 'ng2-qgrid';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static id = 'focus-cell-basic';
+
+	gridModel: GridModel;
+
+	constructor(dataService: DataService, qgrid: Grid) {
+		this.gridModel = qgrid.model();
+
+		dataService
+			.getAtoms()
+			.subscribe(rows => {
+				this.gridModel.data({ rows });
+
+				const gridService = qgrid.service(this.gridModel);
+
+				// navigate to the 2nd page to the bottom
+				gridService.focus(99, 2);
+			});
+	}
+}
