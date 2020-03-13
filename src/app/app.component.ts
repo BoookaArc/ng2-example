6342b3f336026aa56ee031a7d1a7dc87f8046d2f
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 19:46:38 +0300

    example/column-list-loop/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static id = 'column-list-loop';
+
+	rows: Observable<Atom[]>;
+
+	columns = [
+		{
+			key: 'number',
+			title: 'Number'
+		},
+		{
+			key: 'symbol',
+			title: 'Symbol'
+		},
+		{
+			key: 'name',
+			title: 'Name'
+		},
+		{
+			key: 'appearance',
+			title: 'Appearance'
+		}
+	];
+
+	constructor(dataService: DataService) {
+		this.rows = dataService.getAtoms();
+	}
+}
