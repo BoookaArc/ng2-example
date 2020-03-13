38e96b1e8975a49d3d712d8a1b8ab39184c0d654
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 20:15:43 +0300

    example/style-cell-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService, Atom } from '../data.service';
+import { StyleCellContext, Column } from 'ng2-qgrid';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static id = 'style-cell-basic';
+
+	rows: Observable<Atom[]>;
+
+	constructor(dataService: DataService) {
+		this.rows = dataService.getAtoms();
+	}
+
+	styleCell(row: Atom, column: Column, context: StyleCellContext) {
+		if (column.key === 'symbol') {
+			context.class(row.symbol, {
+				'color': `#${row.color}`,
+				'background': '#3f51b5'
+			});
+		}
+	}
+}
