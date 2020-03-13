f8ce30f9768c011030d8a89ab5b308b880607e73
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 19:46:14 +0300

    example/column-list-basic/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static id = 'column-list-basic';
+
+	rows: Observable<Human[]>;
+	group = '';
+
+	constructor(dataService: DataService) {
+		this.rows = dataService.getPeople();
+	}
+}
