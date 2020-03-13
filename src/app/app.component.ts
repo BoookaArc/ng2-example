170aff7c502f961b3514df091fb4cbcec96e975f
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 20:02:31 +0300

    example/index-column-hybrid/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+import { GridModel, Grid } from 'ng2-qgrid';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static id = 'index-column-hybrid';
+
+	gridModel: GridModel;
+
+	constructor(dataService: DataService, qgrid: Grid) {
+		this.gridModel = qgrid.model();
+
+		dataService
+			.getAtoms()
+			.subscribe(rows => {
+				this.gridModel.data({
+					rows,
+					columns: [
+						{
+							key: 'name',
+							title: '[js 3]Name',
+							index: 3
+						},
+						{
+							key: 'source',
+							title: '[js 2]Source',
+							index: 2
+						}, {
+							key: 'melt',
+							title: '[js 8]Melt',
+							index: 8
+						},
+						{
+							key: 'mass',
+							title: '[js no index]Mass'
+						},
+						{
+							key: 'boil',
+							title: '[js 0]Boil',
+							index: 0
+						}
+					]
+				});
+			});
+	}
+}
