7080b65ef74915a25b2be89f2844cc0ac8c98614
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 19:52:45 +0300

    example/details-row-api/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, ChangeDetectionStrategy, AfterViewInit, ViewChild } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+import { Command, GridComponent, RowDetailsStatus } from 'ng2-qgrid';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent implements AfterViewInit {
+	static id = 'details-row-api';
+
+	@ViewChild(GridComponent, { static: true }) grid: GridComponent;
+	rows: Observable<Atom[]>;
+	enableExpand = true;
+
+	expandAllCommand = new Command({
+		execute: () => {
+			const { model } = this.grid;
+			model.row({
+				status: new Map(model.data().rows.map<[Atom, RowDetailsStatus]>(x => [x, new RowDetailsStatus(true)]))
+			});
+		}
+	});
+
+	collapseAllCommand = new Command({
+		execute: () => {
+			const { model } = this.grid;
+			model.row({
+				status: new Map(model.data().rows.map<[Atom, RowDetailsStatus]>(x => [x, new RowDetailsStatus(false)]))
+			});
+		}
+	});
+
+	expandSecondCommand = new Command({
+		execute: () => {
+			this.collapseAllCommand.execute();
+			const { model } = this.grid;
+			model.row({
+				status: new Map([[model.data().rows[1], new RowDetailsStatus(true)]])
+			});
+		}
+	});
+
+	disableExpandAllCommand = new Command({ execute: () => this.enableExpand = false });
+	allowExpandAllCommand = new Command({ execute: () => this.enableExpand = true });
+
+	constructor(dataService: DataService) {
+		this.rows = dataService.getAtoms();
+	}
+
+	ngAfterViewInit() {
+		const { model } = this.grid;
+
+		model.row({
+			toggle: new Command({
+				canExecute: ({ row }) => this.enableExpand
+			})
+		});
+	}
+}
