3c78dbd8ce566e56632fd8b8c80f35e9f9e6fe7f
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 20:07:47 +0300

    example/persistence-server/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { GridModel, Grid } from 'ng2-qgrid';
+
+import { DataService, Atom } from '../data.service';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static id = 'persistence-server';
+
+	gridModel: GridModel;
+	rows: Observable<Atom[]>;
+	currentUser = 'user1';
+	users: Observable<string[]>;
+
+	constructor(private dataService: DataService, private qgrid: Grid) {
+		this.rows = dataService.getAtoms();
+		this.users = dataService.getUsers();
+		this.gridModel = this.qgrid.model();
+		this.gridModel.persistence({
+			storage: this.storage
+		});
+	}
+
+	get storage() {
+		return {
+			getItem: id => {
+				return new Promise(resolve => {
+					this.dataService
+						.getAtomPresets(id, this.currentUser)
+						.subscribe(resolve);
+				});
+			},
+			setItem: (id, items) => {
+				return new Promise(resolve => {
+					this.dataService
+						.setAtomPresets(id, this.currentUser, items)
+						.subscribe(resolve);
+				});
+			}
+		};
+	}
+}
