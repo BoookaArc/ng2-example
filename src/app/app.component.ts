06e9b4d43a8c36a570621b0c706b008dc2294f1d
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 19:57:06 +0300

    example/filter-row-custom/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+import { GridComponent, GridService, Grid, GridModel } from 'ng2-qgrid';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+	static id = 'filter-row-custom';
+
+	@ViewChild(GridComponent, { static: true }) myGrid: GridComponent;
+	rows: Observable<Atom[]>;
+	gridModel: GridModel;
+	gridService: GridService;
+
+	search = {
+		name: '',
+		phase: ''
+	};
+
+	constructor(dataService: DataService, grid: Grid) {
+		this.rows = dataService.getAtoms();
+		this.gridModel = grid.model();
+		this.gridService = grid.service(this.gridModel);
+
+		this.gridModel.navigationChanged.watch(e => {
+			if (e.hasChanges('cell') && e.state.cell) {
+				this.gridModel.selection({
+					items: [e.state.row]
+				});
+			}
+		});
+	}
+
+	filter(name: string, value: string) {
+		this.search[name] = value;
+
+		const predicate = Object
+			.keys(this.search)
+			.reduce((memo: (x: any) => boolean, key) => {
+				const searchText = this.search[key].toLowerCase();
+				if (searchText) {
+					return row => memo(row) && ('' + row[key]).toLowerCase().indexOf(searchText) >= 0;
+				}
+
+				return memo;
+			}, x => true);
+
+		this.gridModel.filter({
+			match: () => predicate
+		});
+	}
+}
