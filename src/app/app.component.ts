cba12eef0d2c903d1f110d1801e56d845e954062
klumba12 <dimitry.sitnov@gmail.com>
2020-03-13 20:17:39 +0300

    example/visibility-model/latest

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, ChangeDetectionStrategy, AfterViewInit, ViewChild } from '@angular/core';
+import { DataService, Atom } from '../data.service';
+import { Observable } from 'rxjs';
+import { GridComponent } from 'ng2-qgrid';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent implements AfterViewInit {
+	static id = 'visibility-model';
+
+	@ViewChild(GridComponent) grid: GridComponent;
+	rows: Observable<Atom[]>;
+
+	constructor(dataService: DataService) {
+		this.rows = dataService.getAtoms();
+	}
+
+	ngAfterViewInit() {
+		const { model } = this.grid;
+
+		model.visibility({
+			toolbar: {
+				bottom: false,
+				left: false,
+				right: false,
+				top: false
+			}
+		});
+	}
+}
